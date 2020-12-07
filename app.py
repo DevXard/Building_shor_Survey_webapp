f57d9b7b3e7d4ceb961e9b1aa353331f96907729
Pavlin <forex.xard@gmail.com>
2020-12-07 14:48:11 -0500

    added sessions and fixed bugg that wud not start the survey

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 import surveys
 
@@ -9,7 +9,7 @@ app.config['SECRET_KEY'] = '123'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-RESPONCES = []
+RESPONCES_key = 'responses'
 
 
 @app.route('/')
@@ -18,15 +18,26 @@ def home_page():
     instructions = call_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
 
+@app.route('/start', methods=['POST'])
+def starting_survey():
+    session[RESPONCES_key] = []
+    
+    return redirect('/surve/0')
+
 @app.route('/surve/<index>')
 def survey_page(index):
     i = int(index)
+    responses = session.get(RESPONCES_key)
     survey_test = call_survey.questions
-    if len(RESPONCES) != i:
-        flash("Please Do not skip Questions", 'error')
-        return redirect(f"/surve/{len(RESPONCES)}")
-    if len(RESPONCES) == i:
+    print(responses)
+    if responses is None:
+        return redirect('/')
+    if len(responses) == len(survey_test):
         return redirect('/thanks')
+    if len(responses) != i:
+        flash("Please Do not skip Questions", 'error')
+        return redirect(f"/surve/{len(responses)}")
+   
     
     
     return render_template('survey.html', surveys=survey_test, index=i)
@@ -38,13 +49,16 @@ def thank_you_page():
 
 @app.route('/awncers/<i>', methods=['POST'])
 def awncers(i):
+    print(session['awncers'])
+    
     awncer = request.form['awncer']
-    RESPONCES.append(awncer)
-    print(RESPONCES)
 
+    responses = session[RESPONCES_key]
+    responses.append(awncer)
+    session[RESPONCES_key] = responses
    
-    if len(RESPONCES) == call_survey.questions:
+    if len(responses) == call_survey.questions:
         return redirect('/thanks')
     else:
-        return redirect(f"/surve/{len(RESPONCES)}")
+        return redirect(f"/surve/{len(responses)}")
    
